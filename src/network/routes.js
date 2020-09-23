5590e5c1bf45ef6b5794c9fac48067673f496dfd
Andres Mesa <andresf.mesad@gmail.com>
2020-09-22 22:22:49 -0500

    [ADD] swagger documentation

--- src/network/routes.js
+++ src/network/routes.js
@@ -1,7 +1,13 @@
 const auth = require('../components/auth/network');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../../swagger.json');
+
 const routes = function(app) {
-    app.use('/auth', auth)
+    app.use('/auth', auth);
+    // Config swagger
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 }
 
 module.exports = routes;
