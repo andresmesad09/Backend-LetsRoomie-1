86d1b2ce4eda81e13acf7dbc2105ccd3d0f700fd
Juan Espitia <juanespitiaoutlook.com>
2020-09-28 22:00:28 -0500

    [ADD] endpoints with isHost true

--- src/components/profiles/network.js
+++ src/components/profiles/network.js
@@ -4,6 +4,7 @@ const profile = require('./controller')
 const auth =  require('../middleware/auth')
 
 router.get('/profile', auth, profile.getProfiles)
+router.get('/profileh', profile.getProfilesIsHost)
 router.post('/profile', auth, profile.addProfile)
 router.get('/profile/:id', auth, profile.getOneProfile)
 router.patch('/profile/:id', auth, profile.updateProfile)
